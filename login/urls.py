d0f512fc4ef45ccea6d2b31d6fab11981462d5b6
Octo-EN <cubemenserver@gmail.com>
2017-09-06 21:43:38 +0100

    Django Login Backend
    Created the backend for the Django Login system. Just needs a bit of redesigning to look better, I'll leave that to the UI/UX designing people.

---
+++ login/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+]
